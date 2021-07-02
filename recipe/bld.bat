2fec0e9df469cc1db31d7d45e5987720c7db52ce
FBT <brandtfa@hu-berlin.de>
2021-07-02 16:50:30 +0200

    Fixes typo, adds workaround for detecting cmake-variables

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,19 @@
-md build
-cd build
+@echo on
 
-cmake -G "Ninja" ^
-    -DCMAKE_BUILD_TYPOE="Release" ^
-    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^ 
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_INSTALL_LIBDIR=lib ..
+mkdir "%SRC_DIR%"\build
+pushd "%SRC_DIR%"\build
+
+cmake -LAH -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_LIBDIR=lib ^
+    ..
 
 if errorlevel 1 exit 1
 
-make
+ninja
 if errorlevel 1 exit 1
 
-make install
+ninja install
 if errorlevel 1 exit 1
